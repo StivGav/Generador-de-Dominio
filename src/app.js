7a1590913c5883d7c3857683b7f121ef687c97dd
StivGav <brandonbsgr@gmail.com>
2025-12-15 17:52:03 +0000

    cambios final

--- src/app.js
+++ src/app.js
@@ -5,7 +5,21 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+
+const pronoun = ['the', 'our'];
+const adj = ['great', 'big'];
+const noun = ['jogger', 'racoon'];
+const domain = ['.com', '.es'];
+
+const dominios= [['the', 'our'],['great', 'big'],['jogger', 'racoon'], ['.com', '.es']];
+
+for (let i =  0; i < dominios[0].length; i++){
+    for (let j = 0; j < dominios[1].length; j++){
+        for (let k = 0; k < dominios[2].length; k++){
+            for (let l = 0; l < dominios[3].length; l++){
+
+                console.log (dominios[0][i] + dominios[1][j] + dominios[2][k] + dominios[3][l]);
+            }
+        }
+    }
+}
