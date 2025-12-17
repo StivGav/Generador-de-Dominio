4336693e8f75246f2a68fc5346f492133091e8f6
StivGav <brandonbsgr@gmail.com>
2025-12-17 20:03:02 +0000

    Versión Mejorada

--- src/app.js
+++ src/app.js
@@ -6,19 +6,18 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 
-const pronoun = ['the', 'our'];
-const adj = ['great', 'big'];
-const noun = ['jogger', 'racoon'];
-const domain = ['.com', '.es'];
+const dominios = [
+    ['the', 'our'],
+    ['great', 'big'],
+    ['jogger', 'racoon'],
+    ['.com', '.es']
+];
 
-const dominios= [['the', 'our'],['great', 'big'],['jogger', 'racoon'], ['.com', '.es']];
-
-for (let i =  0; i < dominios[0].length; i++){
-    for (let j = 0; j < dominios[1].length; j++){
-        for (let k = 0; k < dominios[2].length; k++){
-            for (let l = 0; l < dominios[3].length; l++){
-
-                console.log (dominios[0][i] + dominios[1][j] + dominios[2][k] + dominios[3][l]);
+for (let pronoun of dominios[0]) {
+    for (let adj of dominios[1]) {
+        for (let noun of dominios[2]) {
+            for (let domain of dominios[3]) {
+                console.log(pronoun + adj + noun + domain);
             }
         }
     }
